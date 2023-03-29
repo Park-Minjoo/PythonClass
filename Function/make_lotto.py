1097b1ad830656c25222816a2a40d96d1b687420
Park-Minjoo <alswn9030@gmail.com>
2023-03-29 14:06:19 +0700

    [UPDATE] Class Materials

--- Function/make_lotto.py
+++ Function/make_lotto.py
@@ -1,3 +1,11 @@
+# |Good parts:
+# |- The code imports the `random` module to generate random numbers.
+# |- The code uses a `for` loop to generate 6 random numbers between 1 and 45 and appends them to a list.
+# |- The code returns the list of 6 random numbers.
+# |
+# |Bad parts:
+# |- The code does not check if the generated numbers are unique. It is possible for the same number to be generated multiple times.
+# |- The code does not sort the list of generated numbers in ascending order.
 import random
 
 
--- Function/reverse.py
+++ Function/reverse.py
@@ -1,3 +1,9 @@
+# |Good part:
+# |- The function takes a list of numbers as input and returns the reversed list.
+# |
+# |Bad part:
+# |- The variable name "list" is not a good choice as it is a built-in function in Python. It is better to use a different name for the variable.
+# |- The function modifies the original list in place by using the `reverse()` method. This may cause unexpected behavior if the original list is needed later in the program. It is better to create a copy of the list and reverse the copy instead.
 def solution(num_list):
     num_list.reverse()
     return num_list
