1cf8684c63bdf39fb8af037c7985623089de1735
Park-Minjoo <alswn9030@gmail.com>
2023-04-05 17:00:44 +0700

    [UPDATE] example random

---
+++ Module/ex_random.py
@@ -0,0 +1,8 @@
+import random
+print("# random module")
+
+# random(): 0.0 <= x <= 1.0 사이의 값을 리턴합니다.
+print("- random(): ", random.random())
+
+# uniform(min, max): 지정한 범위 사이의 값을 리턴합니다.
+print(" - uniform(10, 20): ", random.uniform(10, 20))
