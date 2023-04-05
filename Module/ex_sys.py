52dd5c7332a4aa10addbf86dfd2e6080606c9e78
Park-Minjoo <alswn9030@gmail.com>
2023-04-05 17:04:04 +0700

    [UPDATE] example rsys

---
+++ Module/ex_sys.py
@@ -0,0 +1,12 @@
+import sys
+
+# 명령 매개변수를 출력합니다.
+# print(sys.argv)
+# print("---")
+
+# 컴퓨터 환경과 관련된 정보 출력
+# print("getwindowsversion(): ", sys.getwindowsversion())
+# print("---")
+print("copyright: ", sys.copyright)
+print("---")
+print("version: ", sys.version)
