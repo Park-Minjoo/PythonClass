2ff2a4a46844a6b743f2483560fa8c7f3312b1be
Park-Minjoo <alswn9030@gmail.com>
2023-04-05 16:58:02 +0700

    [UPDATE] example math

---
+++ Module/ex_math.py
@@ -0,0 +1,10 @@
+import math
+
+print(math.sin(1))
+print(math.cos(1))
+print(math.tan(1))
+
+print(math.ceil(3.5))  # 올림
+print(math.floor(3.5))  # 내림
+print(round(3.5))  # 반올림
+print(round(3.4))  # 반올림
